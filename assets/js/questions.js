df88071142ef94c3dae212696b8024c27fc9f667
nhodges <hodges27@outlook.com>
2022-02-10 18:00:05 -0500

    questions js

---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+// list of questions, choice, and answers
+var questions = [
+    {
+        title: "Commonly user data types DO NOT include:",
+        choices: ["strings", "boolens", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within _____.",
+        choice: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store _____.",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool user during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "termina; / bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+];
