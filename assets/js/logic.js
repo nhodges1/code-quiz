70e2a1003e6191ed0fd7cc6f4605b6ad0f9a1608
nhodges <hodges27@outlook.com>
2022-02-10 16:56:18 -0500

    Doms

---
+++ assets/js/logic.js
@@ -0,0 +1,13 @@
+// quiz state
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+// DOM elements
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl =document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
