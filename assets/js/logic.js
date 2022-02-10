98bdd569a33e6f424841278fdb0b287582a210bb
nhodges <hodges27@outlook.com>
2022-02-10 17:24:46 -0500

    questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -32,3 +32,73 @@ function startQuiz() {
 
     getQuestion();
 }
+
+function getQuestion() {
+    // get current question from array
+    var currentQuestion = question[currentQuestionIndex];
+
+    // update title with current question
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+
+    // clear old question choices
+    choicesEl.innerHTML = "";
+
+    // loop over choices
+    currentQuestion.choices.forEach(function(choice, i) {
+        // create new button for each choice
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
+
+        choiceNode.textContent = i + 1 +  ". " + choice;
+        
+        // attach click event listener to each choice
+        choiceNode.onclick = questionClick;
+
+        // diplay on the page
+        choicesEl.appendChild(choiceNode);
+    });
+}
+
+function questionClick() {
+    // check if user guessed wrong
+    if (this.value !== questions[currentQuestionIndex].answer) {
+        // deduct time
+        time -= 15;
+
+        if (time < 0) {
+            time = 0;
+        }
+
+        // display new time on page
+        timerEl.textContent = time;
+
+        // play "worng" sound effect
+        sfxWrong.play();
+
+        feedbackEl.textContent = "Wrong!";
+    } else {
+        // play "right" sound effect
+        sfxRight.play();
+
+        feedbackEl.textContent = "Correct!";
+    }
+
+    // show right/wrong feedback on page for half a second
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    // move to next question
+    currentQuestionIndex++;
+
+    // check if we're out of questions
+    if (currentQuestionIndex === questions.length) {
+        quizEnd();
+    }else {
+        getQuestion();
+    }
+}
+
